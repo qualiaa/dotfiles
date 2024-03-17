0739b9720fbb4e7f4c83de40caef201e56a1b0e8
Jamie Bayne <jamiebayne@hotmail.com>
2024-03-17 20:48:30 +0000

    Add restic user service

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -13,4 +13,28 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.printing.drivers = [pkgs.cups-brother-hll2350dw];
+
+  # Configure restic backups
+  systemd.user.services.restic = {
+    path = [ pkgs.openssh ];
+    environment = {
+      RESTIC_REPOSITORY = "sftp:backups:jamie";
+      RESTIC_PASSWORD_FILE = "%h/.restic-password";
+    };
+    description = "Restic backup";
+    requires = ["default.target"];
+    serviceConfig = {
+      ExecStart = ''${pkgs.restic}/bin/restic backup %h --exclude-file %h/.restic-exclude-file'';
+      RestartSec = "30m";
+      Restart = "on-failure";
+    };
+  };
+  systemd.user.timers.restic = {
+    description = "Restic backup timer";
+    timerConfig = {
+      OnCalendar = "Sat 02:00";
+      Persistent = true;
+    };
+    wantedBy = ["timers.target"];
+  };
 }
